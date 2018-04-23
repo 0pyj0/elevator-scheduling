584501a6099f27db777df4ed93fa88f8626badbd
0pyj0 <36054533+0pyj0@users.noreply.github.com>
2018-04-23 21:28:54 +0800

    Update main.cpp

--- elevator-scheduling/main.cpp
+++ elevator-scheduling/main.cpp
@@ -16,18 +16,18 @@ int main()
 		infile>>b[i];
 		infile>>c[i];
 	}
-	s=a[0]+b[0];
+	s=a[0]+b[0];//将第一个请求的起始楼层输出
 	outfile<<s<<" "<<b[0]<<endl;
 	for(i=1;i<n;i++)
 	{
 		s=s+1+dt[i].time(b[i-1],c[i-1]);
-		if(s>=a[i])
+		if(s>=a[i])//当电梯完成上一个请求之前，下一个请求已经开始的时候
 		{
 			s=s+1+dt[i].time(b[i],c[i-1]);
 			b[i+1]=c[i];
 			outfile<<s-1<<" "<<b[i]<<endl;
 		}
-		else
+		else//当上一个请求结束了以后，下一个请求还没出现，先向下一个地点走，当s=a时，再接着计时
 		{
 			if(dt[i].time(b[i],c[i])>(s-a[i]))
 			s=a[i]+1+(dt[i].time(b[i],c[i-1])-s+a[i]);
